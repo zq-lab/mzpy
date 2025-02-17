587276799dfd8b169e63bb2bca58506022bd6882
张强 <6256691@qq.com>
2025-02-17 21:55:04 +0800

    mzMatch update
    mzMatch update

--- mzpy/mzMatch.py
+++ mzpy/mzMatch.py
@@ -1,329 +1,284 @@
-'''
-同一个样本两次测试，相似度超过0.5的很少
-应该能超过0.5的很多才对！
-'''
-
-
-
 import numpy as np
-import pandas as pd
-# 尝试导入 numba，如果失败，则定义一个空装饰器
-try:
-    from numba import njit
-    has_numba = True
-except ImportError:
-    print('Recommend installing numba to accelerate the cosine algorithm.')
-    def njit(*args, **kwargs):
-        def decorator(func):
-            return func  # 返回原始函数，不做任何修改
-        return decorator
-    has_numba = False
- 
-@njit if has_numba else lambda x: x 
-def _cosine_(a, b, tol=0.005):
-    """
-    计算两条 msms 谱 a, b 的“匹配率修正”余弦相似度。
-    a, b: shape (N,2), (M,2)，分别表示 [mz, intensity]，且均为 numpy.ndarray。
-    tol: 峰匹配的公差 (绝对差值)
+from numba import njit
 
-    算法:
-    1) 先按 mz 升序排序两条谱。
-    2) 逐峰比对，当 |mz_a - mz_b| <= tol 视为匹配并记录。
-    3) 计算普通余弦相似度 cos_val = dot_val / (norm_a * norm_b)。
-    4) 匹配率 = matched_count / (n_a + n_b)。
-    5) 最终返回 cos_val * 匹配率。
-    """
-    # 按 mz 升序排序
-    sort_idx_a = np.argsort(a[:, 0])
-    sort_idx_b = np.argsort(b[:, 0])
-    a_sorted = a[sort_idx_a]
-    b_sorted = b[sort_idx_b]
+@njit
+def _cosine_(que_sorted, ref_sorted, tol=(0.003, 0.005)):  
+    """计算余弦相似度的核心逻辑。
+    Assumption: que_sorted and ref_sorted's MS2 are already sorted in descending order by m/z, 
+                and relative abundance has been applied.  
+    
+    Args:  
+        que_sorted (ndarray): mz从大到小排序后的查询数据。  
+        ref_sorted (ndarray): mz从大到小排序后的参考数据。  
+        tol (tuple): 峰匹配的公差，包含两个值。  
+    
+    Returns:  
+        float: 计算得到的余弦相似度或 -1.0。  
+    """  
+    # 检查 precursor mz 差值  
+    mz_diff = abs(que_sorted[0, 0] - ref_sorted[0, 0])  
+    if mz_diff > tol[0]:  
+        return -1.0  
 
-    n_a = a_sorted.shape[0]
-    n_b = b_sorted.shape[0]
+    # 计算 MS2 的余弦相似度  
+    n_que = que_sorted.shape[0]  
+    n_ref = ref_sorted.shape[0]  
 
-    # 准备并集数组(最大长度=n_a+n_b)
-    union_a = np.empty(n_a + n_b, dtype=a.dtype)
-    union_b = np.empty(n_a + n_b, dtype=b.dtype)
+    union_que = np.empty(n_que + n_ref, dtype=que_sorted.dtype)  
+    union_ref = np.empty(n_que + n_ref, dtype=ref_sorted.dtype)  
 
-    i = 0
-    j = 0
-    idx = 0
-    matched_count = 0  # 用于统计峰匹配数量
+    i = 1  # 从第二行开始  
+    j = 1  # 从第二行开始  
+    idx = 0  
 
-    while i < n_a and j < n_b:
-        mz_a, int_a = a_sorted[i]
-        mz_b, int_b = b_sorted[j]
-        if abs(mz_a - mz_b) <= tol:
-            union_a[idx] = int_a
-            union_b[idx] = int_b
-            i += 1
-            j += 1
-            matched_count += 1  # 匹配峰数量+1
-        elif mz_a < mz_b:
-            union_a[idx] = int_a
-            union_b[idx] = 0.0
-            i += 1
-        else:
-            union_a[idx] = 0.0
-            union_b[idx] = int_b
-            j += 1
-        idx += 1
+    while i < n_que and j < n_ref:  
+        mz_que, int_que = que_sorted[i]  
+        mz_ref, int_ref = ref_sorted[j]  
+        if abs(mz_que - mz_ref) <= tol[1]:  
+            union_que[idx] = int_que  
+            union_ref[idx] = int_ref  
+            i += 1  
+            j += 1  
+        elif mz_que > mz_ref:  
+            union_que[idx] = int_que  
+            union_ref[idx] = 0.0  
+            i += 1  
+        else:  
+            union_que[idx] = 0.0  
+            union_ref[idx] = int_ref  
+            j += 1  
+        idx += 1  
 
-    # 补齐剩余
-    while i < n_a:
-        union_a[idx] = a_sorted[i, 1]
-        union_b[idx] = 0.0
-        i += 1
-        idx += 1
+    while i < n_que:  
+        union_que[idx] = que_sorted[i, 1]  
+        union_ref[idx] = 0.0  
+        i += 1  
+        idx += 1  
 
-    while j < n_b:
-        union_a[idx] = 0.0
-        union_b[idx] = b_sorted[j, 1]
-        j += 1
-        idx += 1
+    while j < n_ref:  
+        union_que[idx] = 0.0  
+        union_ref[idx] = ref_sorted[j, 1]  
+        j += 1  
+        idx += 1  
 
-    # 截断到实际并集长度
-    union_a = union_a[:idx]
-    union_b = union_b[:idx]
-
-    # 计算余弦
-    norm_a = np.sqrt(np.sum(union_a * union_a))
-    norm_b = np.sqrt(np.sum(union_b * union_b))
-    if norm_a == 0 or norm_b == 0:
-        return 0.0
-
-    dot_val = np.sum(union_a * union_b)
-    cos_val = dot_val / (norm_a * norm_b)
-
-    # 匹配率修正
-    match_ratio = matched_count / idx if idx > 0 else 0.0
-    return cos_val * match_ratio
-
-
-def warmup():
-    """创建小数组进行热身以预编译 njit 函数"""  
-    ref_ms = np.array([[100.0, 1.0]], dtype=np.float32)
-    _cosine_(ref_ms, ref_ms)  # 直接调用 _cosine_ 函数进行热身
-
-if has_numba:
-    warmup()
-
-
-class Match:
+    union_que = union_que[:idx]  
+    union_ref = union_ref[:idx] 
     '''
-    Performance
-        Calculating internal similarity for 1,200 lines of data, took 0.002519s
+    union_que and union_ref are two arrays used to store the merged intensity values. 
+    Their specific meanings and roles are as follows:
+
+    - union_que: an array used to store the intensity values corresponding to 
+                    each matched mz in the query MS (que).
+        When a mz in `que` matches a mz in `ref`, `union_que` will store the intensity value in `que`.
+    
+    The purpose of `union_que` and `union_ref` is to merge the intensity information of the two spectra 
+        into a unified structure for subsequent cosine similarity calculation.
+    Handling Unmatched Peaks: By setting the unmatched intensity values to 0, 
+        it ensures that the cosine similarity calculation is not affected.
     '''
-    def __init__(self, ref_mz, ref_ms, que_mz=None, que_ms=None, sort_ms=False):
-        """
-        初始化 Match 类：
-          1) 如果 que_mz, que_ms 为空，则认为自对比。
-          2) 将数据转换为 float32 格式。
-          3) 如果需要排序，则按 mz 排序。
+    norm_que = np.sqrt(np.sum(union_que * union_que))  
+    norm_ref = np.sqrt(np.sum(union_ref * union_ref))  
+    if norm_que == 0 or norm_ref == 0:  
+        return 0.0  
+
+    dot_val = np.sum(union_que * union_ref)  
+    cos_val = dot_val / (norm_que * norm_ref)  
+
+    lSpectrumCounter = np.sum(union_ref > 10)  
+
+    if lSpectrumCounter == 1:  
+        peakCountPenalty = 0.75  
+    elif lSpectrumCounter == 2:  
+        peakCountPenalty = 0.88  
+    elif lSpectrumCounter == 3:  
+        peakCountPenalty = 0.94  
+    elif lSpectrumCounter == 4:  
+        peakCountPenalty = 0.97  
+    else:  
+        peakCountPenalty = 1.0  
+
+    return cos_val * peakCountPenalty  
+
+
+class MSmx():
+    '''
+    Encapsulated MS array, 
+        - the first element fixed as the precursor's m/z and intensity (default intensity 100), 
+        - the second and subsequent elements as normalized MS2, 
+            converted to relative abundance and sorted by m/z in descending order.
+    ''' 
+    def __init__(self, precursormz, ms2, precursor_intensity=100):  
+        """  
+        初始化 MSList 实例，创建一个包含母离子和 MS2 数据的数组。  
         
-          
-        """
+        参数:  
+        precursor_intensity: 母离子的强度，默认为 100  
+        precursormz: 母离子的质荷比 (float)  
+        ms2: MS2 的二维数组，形状为 (N, 2)，表示 [mz, intensity]  
+        """  
 
-        # 如果 que_mz, que_ms 为空，则认为自对比
-        self.is_symmetric = (que_mz is None or len(que_mz) == 0) and (que_ms is None or len(que_ms) == 0)
+        # 创建包含母离子质荷比和强度的数组  
+        precursor_array = np.array([[precursormz, precursor_intensity]])  
 
-        # 转换为 float32
-        self.ref_mz = np.asarray(ref_mz, dtype=np.float32)
-        self.ref_ms = []
-        for item in ref_ms:
-            arr = np.asarray(item, dtype=np.float32)
-            if sort_ms:
-                idx = np.argsort(arr[:, 0])
-                arr = arr[idx]
-            self.ref_ms.append(arr)
+        # 将 MS2 数据转为相对丰度后按 mz 从大到小排序 
+        ms2 = np.asarray(ms2) 
+        max_intensity_ms2 = np.max(ms2[:, 1])   
+        if max_intensity_ms2 != 100:  
+            ms2[:, 1] = (ms2[:, 1] / max_intensity_ms2) * 100  # 转换为相对百分比  
 
-        if self.is_symmetric:
-            self.que_mz = None
-            self.que_ms = None
-        else:
-            self.que_mz = np.asarray(que_mz, dtype=np.float32)
-            self.que_ms = []
-            for item in que_ms:
-                arr = np.asarray(item, dtype=np.float32)
-                if sort_ms:
-                    idx = np.argsort(arr[:, 0])
-                    arr = arr[idx]
-                self.que_ms.append(arr)
+        sorted_ms2 = ms2[np.argsort(ms2[:, 0])[::-1]]  # 从大到小排序  
 
-    def cosine_mx(self, tol=(0.003, 0.005)):
-        """
-        计算两批母离子 ref_mz, query_mz 的匹配后 msms 余弦相似度矩阵 (匹配率修正版本)。
+        # 将母离子数组和排序后的 MS2 数据合并  
+        data = np.vstack((precursor_array, sorted_ms2))  
+        self.data = np.ascontiguousarray(data)
 
-        tol=(match_tol, msms_tol):
-           tol[0] -> 母离子 mz 匹配公差
-           tol[1] -> msms 峰比对时的 mz 公差
 
-        返回
-        ----------
-        sim_matrix : numpy.ndarray, shape (R, Q)
-            sim_matrix[i, j] = 当 |ref_mz[i] - query_mz[j]| <= tol[0] 时，计算匹配率修正的余弦相似度；
-                               否则为 0.0。
-        """
-        if self.is_symmetric:
-            que_mz = self.ref_mz  # 这是临时赋值，不要去更改 self.que_mz and self.que_ms
-            que_ms = self.ref_ms
-        else:
-            que_mz = self.que_mz
-            que_ms = self.que_ms
+    def __getitem__(self, index):  
+        """  
+        支持索引访问，返回指定索引的元素。  
+        """  
+        return self.data[index]  
 
-        R = self.ref_mz.shape[0]
-        Q = que_mz.shape[0]
-        similarity = np.full((R, Q), -1.0, dtype=np.float32)
-        # 广播比对母离子
-        diff_mz = np.abs(self.ref_mz[:, None] - que_mz[None, :])
-        matched_mask = diff_mz <= tol[0]
-        matched_i, matched_j = np.where(matched_mask)
+    def __len__(self):  
+        """  
+        返回 MSList 中元素的数量。  
+        """  
+        return len(self.data)  
 
-        if self.is_symmetric:
-            # 只保留上三角(含对角线)
-            upper_mask = matched_j >= matched_i
-            matched_i = matched_i[upper_mask]
-            matched_j = matched_j[upper_mask]
+    def __repr__(self):  
+        """  
+        返回 MSList 的字符串表示。  
+        """  
+        return f"MSList({self.data})"
 
-            # 对角线初始化为 1.0
-            diag_idx = np.arange(R)
-            similarity[diag_idx, diag_idx] = 1.0
+    def compute_similarity(self, ms_mx, tol=(0.003, 0.005)):
+        if not isinstance(ms_mx, self.__class__):
+            raise TypeError(f'ref_ms_list must be an instance of {self.__class__.__name__}')
+        
+        if abs(self.precursormz - ms_mx.precursormz) > tol[0]:
+            return -1.0
+        return _cosine_(self.msms, ms_mx.msms, tol[1])
 
-            # 遍历 matched_i, matched_j 完成上三角
-            for k in range(matched_i.shape[0]):
-                i = matched_i[k].item()
-                j = matched_j[k].item()
-                if i == j:
-                    continue  # 对角线已是 1.0, 需要可改成真实计算则移除此行
-                val = _cosine_(self.ref_ms[i], que_ms[j], tol[1])
-                similarity[i, j] = val
-                similarity[j, i] = val  # 镜像
-        else:
-            for k in range(matched_i.shape[0]):
-                i = matched_i[k].item()
-                j = matched_j[k].item()
-                val = _cosine_(self.ref_ms[i], que_ms[j], tol[1])
-                similarity[i, j] = val
 
-        return similarity
+    @property
+    def ms2_mz(self):
+        return self.data[1:, 0]
 
-    @classmethod
-    def cosine_df_chunk(cls, df, num_chunks, csv_fname, mz_on='precursormz', ms_on='msms', tol=(0.003, 0.005),
-                       score_thd=0.0,
-                       mode='w'):
+    @property
+    def ms2_int(self):
+        self.data[1:, 1]
+
+    @property
+    def split_ms2(self):
+        self.data[1:, 0], self.data[1:, 1]
+
+    @property
+    def msms(self):
+        return self[1:]    
+    
+    @property
+    def num_ms2(self):
+        return self.data.shape[0]-1
+
+    @property
+    def precursor(self):
+        return self[0]
+
+    @property
+    def precursormz(self):
+        return self[0][0] 
+
+
+
+class MSList:  
+    def __init__(self, precursormz_list, msms_list, precursor_intensity=100):
+        """  
+        初始化 MSList 实例，创建一个包含母离子和对应子离子信息的数组。  
+        
+        参数:  
+        precursormz_list: 母离子的质荷比列表 (list of float)  
+        msms_list: 对应的子离子信息列表，每个元素为二维数组 (list of numpy.ndarray)  
+        precursor_intensity: 母离子的强度，默认为 100  
+        """  
+        if len(precursormz_list) != len(msms_list):  
+            raise ValueError("The length of precursormz_list must match the length of msms_list.") 
+
+        msms_list = [np.asarray(msms)  for msms in msms_list] 
+
+        # 创建一个空的列表来存储每个母离子及其子离子信息  
+        self.data = []  
+
+        for precursormz, msms in zip(precursormz_list, msms_list):  
+            # 创建包含母离子质荷比和强度的数组  
+            precursor_array = np.array([[precursormz, precursor_intensity]])  
+            # 合并母离子数组和子离子信息  
+
+            # 将 MS2 数据转为相对丰度后按 mz 从大到小排序
+            max_intensity_ms2 = np.max(msms[:, 1])   
+            if max_intensity_ms2 != 100:  
+                msms[:, 1] = (msms[:, 1] / max_intensity_ms2) * 100  # 转换为相对百分比  
+
+            sorted_msms = msms[np.argsort(msms[:, 0])[::-1]]  # 从大到小排序
+
+            combined_array = np.vstack((precursor_array, sorted_msms))  
+            self.data.append(combined_array)  
+
+        # 将列表转换为 NumPy 数组  
+        self.data = np.array(self.data, dtype=object)  # 使用 dtype=object 以支持不同形状的数组  
+
+    def __getitem__(self, index):  
+        """  
+        支持索引访问，返回指定索引的元素。  
+        """  
+        return self.data[index]  
+
+    def __len__(self):  
+        """  
+        返回 MSList 中元素的数量。  
+        """  
+        return len(self.data)  
+
+    def __repr__(self):  
+        """  
+        返回 MSList 的字符串表示。  
+        """  
+        return f"MSList({self.data})" 
+    
+    def to_cupy(self):  
+        """  
+        将数据转换为 CuPy 数组并返回。  
+        
+        返回:  
+        list of cupy.ndarray: 每个母离子及其子离子信息的 CuPy 数组列表  
+        """  
+        import cupy 
+        cupy_data = []  
+        for ms_array in self.data:  
+            # 将 NumPy 数组转换为 CuPy 数组  
+            cupy_array = cupy.asarray(ms_array)  # 使用 cp.asarray 进行转换  
+            cupy_data.append(cupy_array)  
+        return cupy_data 
+    
+    def to_numpy(self):
+        return np.ascontiguousarray(self.data)
+    
+    def compute_similarity(self, ref_ms_list, tol=(0.003, 0.005)):
         '''
-        超大数据表内部的MS相似度计算
-        分块推进
-        之所以按块推进，是为了充分使用内存，若按行逐行向下计算，由于来回转储，时间开销大
-        注意：返回的数据框也非常长，如果一致存储在内存，需要TB级的内存       
-        性能：1200行数据，需要一定的时间
-
-        客户端在获得score长表后，最好按阈值过滤行，然后存储，以避免硬盘空间不够
-        为方便后续的数据处理，客户端最好同时保存df和score，可以对应着过滤数据
+        compoute cosine similarity between self list and ref_ms_list
+        ref_ms_list, an instance of MSList
+        return:
+            a matrix of cosine similarity
         '''
-        chunks = np.array_split(df.index, num_chunks)
-
-        n = len(chunks)
-        f = open(csv_fname, mode=mode)
-        f.write('row,col,score\n')
-        f.flush()
-
-        for i, chk in enumerate(chunks):
-            # 计算当前块内部的相似度矩阵，转换为相似度长表
-            que = df.loc[chk]
-            match = cls.create_from_df(que, mz_on=mz_on, ms_on=ms_on)
-            score = match.cosine_mx(tol=tol)
-            score_df = match.to_long_df(que.index, que.index, score)
-            score_df = score_df[(score_df['row'] < score_df['col']) &
-                                (score_df['score'] > score_thd)]
-            score_df.to_csv(f, mode='a', header=False, index=False)
-            f.flush()
-
-            if i < n - 1:
-                # 计算当前块与其后所有块数据的相似度，转换为相似度长表
-                # 下一个chunk的第一个索引值作为起点
-                ref = df.loc[chunks[i + 1][0]:]
-                match = cls.create_from_df(ref, que,
-                                           mz_on=mz_on,
-                                           ms_on=ms_on)
-                score = match.cosine_mx(tol=tol)
-                score_df = match.to_long_df(ref.index, que.index, score)
-                score_df = score_df[score_df['score'] > score_thd]
-                score_df.to_csv(f, mode='a', header=False, index=False)
-                f.flush()
-
-        f.close()
-
-    @classmethod
-    def create_from_df(cls, ref, que=None,
-                       mz_on='precursormz',
-                       ms_on='msms',
-                       ref_mz_on=None,
-                       ref_ms_on=None,
-                       que_mz_on=None,
-                       que_ms_on=None,
-                       sort_ms=False
-                       ):
-        '''
-        create objec from mzFrame
-        '''
-        if ref_mz_on is None:
-            ref_mz_on = mz_on
-        if ref_ms_on is None:
-            ref_ms_on = ms_on
-        if que_mz_on is None:
-            que_mz_on = mz_on
-        if que_ms_on is None:
-            que_ms_on = ms_on
-        if que is None:
-            return cls(ref[ref_mz_on].values, ref[ref_ms_on].to_list(), sort_ms=sort_ms)
-        else:
-            return cls(ref[ref_mz_on].values, ref[ref_ms_on].to_list(),
-                       que[que_mz_on].values, que[que_ms_on].to_list(),
-                       sort_ms=sort_ms)
-
-    def to_long(self, score_mx):
-        '''
-        transform score format
-        '''
-        rows, cols = np.indices(score_mx.shape)
-        return rows.ravel(), cols.ravel(), score_mx.ravel()
-
-    def to_long_df(self, ref_index, que_index, score_mx):
-        """
-        通过 ref_index, query_index 和一个二维 scores 矩阵构建一个 DataFrame。
-        其中：
-            - ref_index 为行索引（一维，可视作形状 n）
-            - query_index 为列索引（一维，可视作形状 m）
-            - scores 为大小 n*m 的 2D 数组或矩阵
-
-        返回的 score_df 有三个列：
-            - ref_index: 对应原 ref_index 中的值
-            - query_index: 对应原 query_index 中的值
-            - score: scores 矩阵中的对应元素
-        """
-        rows, cols, scores = self.to_long(score_mx)
-
-        score_df = pd.DataFrame({
-            "row": ref_index[rows].to_list(),
-            "col": que_index[cols].to_list(),
-            "score": np.round(scores, decimals=4)
-        })
-        return score_df
-
-    def to_csv(self, score_mx, filename, mode='w'):
-        '''
-        save similarity matrix to a csv file with three columns:
-            row, col, value
-        '''
-        data = self.to_long(score_mx)
-        if mode.lower() in ("a", 'append'):
-            with open(filename, mode, newline='') as f:
-                np.savetxt(f, np.column_stack(data), delimiter=',', fmt='%s,%s,%f')
-        else:
-            with open(filename, mode, newline='') as f:
-                f.write('row,col,value\n')
-                np.savetxt(f, np.column_stack(data), delimiter=',', fmt='%s,%s,%f')
+        if not isinstance(ref_ms_list, self.__class__):
+            raise TypeError(f'ref_ms_list must be an instance of {self.__class__.__name__}')
+        
+        n_que = self.data.shape[0]  
+        n_ref = ref_ms_list.data.shape[0]  
+        similarity_matrix = np.full((n_que, n_ref), -1.0)  # 初始化相似度矩阵，默认值为 -1.0  
 
 
+        for i in range(n_que):  
+            for j in range(n_ref):  
+                similarity_matrix[i, j] = self._cosine_(self.data, ref_ms_list.data, tol)  
+
+        return similarity_matrix      
